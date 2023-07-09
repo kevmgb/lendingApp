5f6b3eaf95a777640c51b309aa9058dbd7c01a7a
kevmgb <kithinjimkevin@gmail.com>
2023-07-09 19:19:18 +0300

    initial commit

---
+++ src/test/java/com/example/lendingApp/LendingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lendingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
